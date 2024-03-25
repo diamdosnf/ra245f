784f7ed932ac12fddb14c81cc77c81a2ae576336
NoNameBuilder <163744362+NoNameBuilder@users.noreply.github.com>
2024-03-25 20:43:21 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,17 +1,17 @@
-#!/bin/bash env
+#!/bin/bash
 
-# about the custom recovery
-export NAME="twrp" # supported: pbrp, shrp, twrp
-export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp" # the link of manifest
-export BRANCH="twrp-12.1" # the branch of manifest
+# About the custom recovery
+export NAME="twrp" # Supported: pbrp, shrp, twrp
+export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp" # The link of manifest
+export BRANCH="twrp-12.1" # The branch of manifest
 
-# about your device
-export DEVICE="a24" # codename used in device tree
-export DT_LINK="https://github.com/twrpdtgen/android_device_samsung_a24/tree/a24nsxx-user-12-SP1A.210812.016-A245FXXS2AWE3-release-keys" # device tree link
-export DT_BRANCH="a24nsxx-user-12-SP1A.210812.016-A245FXXS2AWE3-release-keys" # device tree branch
-export VENDOR="samsung" # device manufacturer or vendor
-export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
-export TYPE="twrp" # recovery type: omni, twrp, something else
+# About your device
+export DEVICE="a24" # Codename used in device tree
+export DT_LINK="https://github.com/twrpdtgen/android_device_samsung_a24/tree/a24nsxx-user-12-SP1A.210812.016-A245FXXS2AWE3-release-keys" # Device tree link
+export DT_BRANCH="a24nsxx-user-12-SP1A.210812.016-A245FXXS2AWE3-release-keys" # Device tree branch
+export VENDOR="samsung" # Device manufacturer or vendor
+export TARGET="recoveryimage" # Build method: recoveryimage, bootimage, vendorboot
+export TYPE="twrp" # Recovery type: omni, twrp, something else
 
-# you dont need to edit these
+# You don't need to edit these
 export SYM="_"

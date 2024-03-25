0fa69224c6c5c982dcef1b6fbafcb9a62c3990f6
NoNameBuilder <163744362+NoNameBuilder@users.noreply.github.com>
2024-03-25 21:52:20 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # About the custom recovery
-export NAME="pbrp" # Supported: pbrp, shrp, twrp
-export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp" # The link of manifest
+export NAME="twrp" # Supported: pbrp, shrp, twrp
+export MANIFEST="https://github.com/NoNameBuilder/twrp" # The link of manifest
 export BRANCH="twrp-12.1" # The branch of manifest
 
 # About your device
 export DEVICE="a24" # Codename used in device tree
-export DT_LINK="https://github.com/twrpdtgen/android_device_samsung_a24" # Device tree link
-export DT_BRANCH="a24nsxx-user-12-SP1A.210812.016-A245FXXU3BWK3-release-keys" # Device tree branch
+export DT_LINK="https://github.com/NoNameBuilder/a24" # Device tree link
+export DT_BRANCH="A245FXXU3BWK3" # Device tree branch
 export VENDOR="samsung" # Device manufacturer or vendor
 export TARGET="recoveryimage" # Build method: recoveryimage, bootimage, vendorboot
 export TYPE="twrp" # Recovery type: omni, twrp, something else
